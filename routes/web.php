47a91dbe1e499d741321bd2d0672887d68393709
Rakesh Chaudhary <rockychaudhary2222@gmail.com>
2022-06-26 15:02:50 +0545

    update

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,40 @@
 <?php
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductCategoryController;
+use App\Http\Controllers\ProductSubCategoryController;
 
-use Illuminate\Support\Facades\Route;
+	
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[IndexController::class,'index'])->name('/');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/product',[ProductController::class,'index'])->name('/product');
+    Route::get('/add-product',[ProductController::class,'create'])->name('/add-product');
+    Route::post('/store-product',[ProductController::class,'store'])->name('/store-product');
+    Route::post('/edit-product/{id}',[ProductController::class,'edit'])->name('edit.index');
+    Route::delete('/delete-product/{id}',[ProductController::class,'destroy'])->name('destroy.index');
+    Route::put('/update-product/{id}',[ProductController::class,'update'])->name('update.index');
+    Route::get('/productCategory',[ProductCategoryController::class,'index'])->name('/productCategory');
+    Route::get('/add-category',[ProductCategoryController::class,'create'])->name('/add-category');
+    Route::post('/store-category',[ProductCategoryController::class,'store'])->name('/store-category');
+    Route::get('/productSubCategory',[ProductSubCategoryController::class,'index'])->name('/productSubCategory');
+    Route::get('/add-subcategory',[ProductSubCategoryController::class,'create'])->name('/add-subcategory');
+    Route::post('/store-subcategory',[ProductSubCategoryController::class,'store'])->name('/store-subcategory');
+    Route::get('/edit-subcategory/{id}',[ProductSubCategoryController::class,'edit'])->name('edit.subcategory');
+    Route::put('/update-subcategory/{id}',[ProductSubCategoryController::class,'update'])->name('update.subcategory');
+    Route::delete('/delete-subcategory/{id}',[ProductSubCategoryController::class,'destroy'])->name('destroy.subcategory');
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
-require __DIR__.'/auth.php';
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
